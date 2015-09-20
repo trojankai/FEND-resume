9336a6060d38fbf5eef446ba12436044300251aa
trojankai <trojankai@gmail.com>
2015-09-19 21:34:17 -0700

    changes to resume builder.js
    changes made while working through the videos in the JS basics course

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,50 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+
+/*$("#main").append(["Kaisha Ansley-Gutierrez"]);
+
+var awesomeThoughts = "I am Kaisha and I am AWESOME!";
+
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
+
+console.log(funThoughts);*/
+var name = "Kaisha Ansley-Gutierrez";
+var role = "Front End Web Developer";
+var email = 'trojankai@gmail.com';
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+var formattedEmail = HTMLemail.replace("%data%", email);
+/*$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+*/
+var bio = {
+    'name': formattedName,
+    'role': formattedRole,
+    'email': formattedEmail,
+    'pictureUrl': 'images/fry.jpg',
+    'welcome-message': 'Welcome to mypage',
+    'skills': 'images/fry.jpg'
+    
+};
+
+$("#header").append(bio.name);
+$("#header").append(bio.role);
+$("#header").append(bio.email);
+
+var work = {};
+work.city = "Whittier";
+work.position = "Freelance Web/Graphic Designer and Stay-at-Home Mother";
+
+$('#header').append(work.city);
+
+$("#header").append(bio.contactInfo);
