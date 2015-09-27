fc6f3d957c8ab67a87fe3567a4da9f1842a28088
trojankai <trojankai@gmail.com>
2015-09-26 21:24:18 -0700

    changes made to make objects with JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,60 +2,12 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-
-
-/*$("#main").append(["Kaisha Ansley-Gutierrez"]);
-<<<<<<< HEAD
-var awesomeThoughts = "I am Kaisha and I am AWESOME!";
-console.log(awesomeThoughts);
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-$("#main").append(funThoughts);
-=======
-
-var awesomeThoughts = "I am Kaisha and I am AWESOME!";
-
-console.log(awesomeThoughts);
-
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-$("#main").append(funThoughts);
-
->>>>>>> origin/master
-console.log(funThoughts);*/
-var name = "Kaisha Ansley-Gutierrez";
-var role = "Front End Web Developer";
-var email = 'trojankai@gmail.com';
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-var formattedEmail = HTMLemail.replace("%data%", email);
-/*$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-*/
-<<<<<<< HEAD
-
-/*var bio = {
-=======
-var bio = {
->>>>>>> origin/master
-    'name': formattedName,
-    'role': formattedRole,
-    'email': formattedEmail,
-    'pictureUrl': 'images/fry.jpg',
-    'welcome-message': 'Welcome to mypage',
-    'skills': 'images/fry.jpg'
-    
-<<<<<<< HEAD
-};*/
-
 var bio = {
     "name": "Kaisha Ansley-Gutierrez",
-    "role": "formattedRole",
+    "role": "Front End Web Developer",
     "contacts": {
         "mobile": "310-343-0537",
-        "email": "formattedEmail",
+        "email": "trojankai@gmail.com",
         "github": "trojankai",
         "twitter": "trojankai",
         "location": "Whittier, CA"
@@ -64,31 +16,85 @@ var bio = {
     "skills": [],
     "biopic": "images/fry.jpg",
     "display": {}
-}
-    
-    
-    
-    
-}
-
-
-
-=======
 };
->>>>>>> origin/master
+var education = {
 
-$("#header").append(bio.name);
-$("#header").append(bio.role);
-$("#header").append(bio.email);
-
-var work = {};
-work.city = "Whittier";
-work.position = "Freelance Web/Graphic Designer and Stay-at-Home Mother";
-
-$('#header').append(work.city);
-
-<<<<<<< HEAD
-$("#header").append(bio.contactInfo);
-=======
-$("#header").append(bio.contactInfo);
->>>>>>> origin/master
+    "schools": [
+		{
+		"name": "University of Southern California",
+		"city": "Los Angeles, CA",
+		"degree": "BA",
+		"major": "Social Sciences emphasis Psychology",
+		"dates":"",
+		"schoolUrl": "www.usc.edu"
+		},
+		{
+		"name": "University of California, Irvine, Extension Program",
+		"major": "Light Construction and Development Management courses",
+		"dates": "",
+		"schoolUrl":"",
+			
+		},
+		{
+		"name": "California State University, Dominguez Hills",
+		"major": "Project Management courses",
+		"dates":"",
+		}
+	],
+	"onlineCourses": [
+		{
+		"name": "Udacity",
+		"courses": ["HTML and CSS","Javascript Basics","Responsive Web Design"]
+		},
+		{
+		"name": "Coursera",
+		"courses": "Introduction to Interactive Programming in Python"
+		}
+	
+	]
+};
+var work = {
+	"jobs":[
+		{
+		"employer":"University of California, Irvine",
+		"title": "Senior Building Maintenance Worker",
+		"location": "Irvine, CA",
+		"dates": "July 2007 to January 2014",
+		"description": "Perform general building maintenance, including carpentry, electrical, plumbing, drywall repair, painting, and basic HVAC.  Maintain good customer service, both internal and external. Report work completed both orally and written. Assist team members as needed."
+		},
+		{
+		"employer":"Bally Total Fitness",
+		"title": "(Acting) Supervisor- Member Services",
+		"location": "Norwalk, CA",
+		"dates": "Feb 2006 to June 2007",
+		"description": "Call center environment-handled member concerns, handled escalated (irate) calls. Assisted in the training and development of member service representatives. Responsible for motivating representatives to meet and maintain company/ departmental standards. Responsible for issuing corrective actions or terminating employees when necessary. Evaluated and corrected timesheets to ensure payroll completed on-time."
+		},
+		{
+		"employer":"Campus Cruiser",
+		"title": "Driver, Dispatcher, Trainer, Field Supervisor",
+		"location": "University of Southern California, Los Angeles, CA",
+		"dates": "Oct 2006 to Jan 2006",
+		"description": "Trained incoming employees to work in the call center, as well as handle dispatched calls while maintaining impeccable customer service and providing safety to patrons. Opened and closed shifts, inspected vehicles for damage, patrolled field to insure that policies and procedures are properly followed by cruisers(drivers). Duties also included handling complaints and special calls, such as disabled patrons requiring wheelchair assistance."
+		}
+	]
+};
+var projects = {
+	"projects":[
+		{
+		"title":"",
+		"dates":"",
+		"description":"",
+		"images":"",		
+		},
+		{
+		"title":"",
+		"dates":"",
+		"description":"",
+		"images":"",
+		}
+	]
+}
+			
+		
+		
+		
