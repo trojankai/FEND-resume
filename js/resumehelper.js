115e422d8fc7fa2493a980456c8a992ee480a499
trojankai <trojankai@gmail.com>
2015-10-19 23:39:54 -0700

    added nav bar
    added a nav bar and tried my hand at making it responsive

--- js/resumehelper.js
+++ js/resumehelper.js
@@ -62,6 +62,235 @@ var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
 
 
+
+// this is the start of resumebuilder.js
+
+//bio object to go to header
+var bio = {
+    "name": "Kaisha Ansley-Gutierrez",
+    "role": "Front End Web Developer",
+    "contacts": {
+        "mobile": "310-343-0537",
+        "email": "trojankai@gmail.com",
+        "github": "trojankai",
+        "twitter": "trojankai",
+        "location": "Whittier, CA"
+    },
+    "welcomeMessage": "I am Hardworking and Incredible",
+    "skills": ["HTML and CSS", "knitting and crocheting", "baby wranglin'" , "learning quickly", "customer service", "time management",  "bath renovations"],
+    "biopic": "images/profilePic.jpg",
+    display: function(){
+      var formattedName = HTMLheaderName.replace("%data%", bio.name);
+      var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+      var formattedNameRole = formattedName + formattedRole;
+      $("#header").prepend(formattedNameRole);
+
+      var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+      var formattedWelcome = HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage);
+
+
+      $("#header").append(formattedBioPic);
+
+      var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+      $("#topContacts").append(formattedLocation);
+      $('#footerContacts').append(formattedLocation);
+
+      var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+      $("#topContacts").append(formattedEmail);
+      $('#footerContacts').append(formattedEmail);
+
+      var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+      $("#topContacts").append(formattedGithub);
+      $('#footerContacts').append(formattedGithub);
+
+      $('#header').append(formattedWelcome);
+
+      $("#header").append(HTMLskillsStart);
+      for (var i = 0; i < bio.skills.length; i++) {
+          var formattedSkills = HTMLskills.replace("%data%", bio.skills[i]);
+           $("#skills-h3").append(formattedSkills);
+    }
+
+}
+};
+
+bio.display();
+
+//education object and dispaly function
+var education = {
+
+    "schools": [
+		{
+		"name": "University of Southern California",
+		"location": "Los Angeles, CA",
+		"degree": "B.A.",
+		"majors": ["Social Sciences emphasis Psychology"],
+		"dates": 'August 2001-Jan 2006 and Jan - May 2012',
+		"url": "www.usc.edu"
+		},
+		{
+		"name": "University of California, Irvine, Extension Program",
+		"majors": ["Light Construction and Development Management courses"],
+		"degree": "Certificate",
+    "location": "Irvine, CA",
+		"dates": 2009 + " - incomplete",
+		"url": "www.unex.uci.edu"
+
+		},
+		{
+		"name": "California State University, Dominguez Hills",
+		"majors": ["Project Management courses"],
+		"degree": "Certificate",
+		"location": "Carson, CA",
+		"dates": 2009 + " - complete program incomplete",
+		"url": "www.csudh.edu"
+		}
+	 ],
+	 "onlineCourses": [
+		{
+		"school": "Udacity",
+		"title": "Front End Web Development ",
+		"dates": "August 2015 - present",
+		"url": "www.udacity.com/nanodegree"
+		},
+		{
+		"school": "Coursera",
+		"title": "Introduction to Interactive Programming in Python",
+		"dates": "July 2015 - October 2015",
+		"url": "www.coursera.com"
+		}
+	],
+
+  display: function() {
+      for (var e = 0; e < education.schools.length; e++) {
+        $("#education").append(HTMLschoolStart);
+        var formattedName = HTMLschoolName.replace("%data%", education.schools[e].name);
+        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[e].location);
+        
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[e].degree);
+        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[e].dates);
+        $(".education-entry:last").append(formattedName + formattedDegree);
+        $(".education-entry:last").append(formattedDates);
+        $(".education-entry:last").append(formattedLocation);
+        var formattedMajors = HTMLschoolMajor.replace("%data%", education.schools[e].majors);
+        $(".education-entry:last").append(formattedMajors);
+
+      }},
+  displayOnline: function() {
+      if (education.onlineCourses.length > 0) {
+        $("#education").append(HTMLonlineClasses);
+        for (var i = 0; i < education.onlineCourses.length; i++) {
+          $("#education").append(HTMLschoolStart);
+          var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
+          var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+          var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].dates);
+          var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url);
+          $(".education-entry:last").append(formattedTitle + formattedSchool);
+          $(".education-entry:last").append(formattedDates);
+          $(".education-entry:last").append(formattedURL);
+          $(".education-entry:last").append("<br>");
+        }
+      }
+    }
+    
+  };
+
+  education.display();
+  education.displayOnline();
+
+
+//work object
+var work = {
+	"jobs": [
+		{
+		"employer":"University of California, Irvine",
+		"title": "Senior Building Maintenance Worker",
+		"location": "Irvine, CA",
+		"dates": "July 2007 to January 2014",
+		"description": "Perform general building maintenance, including carpentry, electrical, plumbing, drywall repair, painting, and basic HVAC.  Maintain good customer service, both internal and external. Report work completed both orally and written. Assist team members as needed."
+		},
+		{
+		"employer":"Bally Total Fitness",
+		"title": "(Acting) Supervisor, Member Services",
+		"location": "Norwalk, CA",
+		"dates": "Feb 2006 to June 2007",
+		"description": "Call center environment-handled member concerns, handled escalated calls. Assisted in the training and development of member service representatives. Responsible for motivating representatives to meet and maintain company/ departmental standards. Responsible for issuing corrective actions or terminating employees when necessary. Evaluated and corrected timesheets to ensure payroll completed on-time."
+		},
+		{
+		"employer": "USC Campus Cruiser",
+		"title": "Driver, Dispatcher, Trainer, Field Supervisor",
+		"location": "Los Angeles, CA",
+		"dates": "Oct 2002 to Jan 2006",
+		"description": "Trained incoming employees to work in the call center, as well as handle dispatched calls while maintaining impeccable customer service and providing safety to patrons. Opened and closed shifts, inspected vehicles for damage, patrolled field to insure that policies and procedures are properly followed by cruisers(drivers). Duties also included handling complaints and special calls, such as disabled patrons requiring wheelchair assistance."
+		}
+	],
+  display: function(){
+    $('#workExperience').append(HTMLworkStart);
+    for(var i=0; i<work.jobs.length; i++){
+      var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[i].employer);
+      var formattedTitle = HTMLworkTitle.replace('%data%',work.jobs[i].title);
+      var formattedEmployerTitle= formattedEmployer + formattedTitle;
+      var formattedDates = HTMLworkDates.replace('%data%', work.jobs[i].dates);
+      var formattedLocation = HTMLworkLocation.replace('%data%',work.jobs[i].location);
+      var formattedDescription = HTMLworkDescription.replace('%data%', work.jobs[i].description);
+      $('.work-entry:last').append(formattedEmployerTitle, formattedDates, formattedLocation, formattedDescription);
+    }
+
+  }
+};
+//display work function...
+
+work.display();
+
+
+//project object
+var projects = {
+	"project":[
+		{
+		"title":"Project 0",
+		"dates":"Aug 2015",
+		"description":"Started the journey of becoming a Front End Developer",
+		"images":["images/project1.jpg"]
+		},
+		{
+		"title":"Re-branding an Elementary School",
+		"dates":"Jan 2015",
+		"description":"Designed a new logo for Dolores Huerta Elementary school",
+		"images":["images/DHES-1.png"]
+		}
+	],
+  display: function(){
+    $('#projects').append(HTMLprojectStart);
+    for(var i = 0; i < projects.project.length; i++){
+      var formattedProjectName = HTMLprojectTitle.replace('%data%', projects.project[i].title);
+      var formattedProjectDates = HTMLprojectDates.replace('%data%', projects.project[i].dates);
+      var formattedProjectDescription = HTMLprojectDescription.replace('%data%', projects.project[i].description);
+      var formattedProjectImage = HTMLprojectImage.replace('%data%', projects.project[i].images);
+      $('.project-entry').append(formattedProjectName, formattedProjectDates, formattedProjectDescription, formattedProjectImage);
+    }
+
+
+  }
+};
+
+projects.display();
+
+
+//internationalize button code--linked above
+function inName(name) {
+    name = name.trim().split(" ");
+    console.log(name);
+    name[1] = name[1].toUpperCase();
+    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+
+    return name[0] + " "+name[1];
+
+}
+
+//$('#main').append(internationalizeButton);
+$('#mapDiv').append(googleMap);
+
+//helper.js information...appended to resumebuiler info so variables are used after they are declared
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
 */
@@ -117,12 +346,12 @@ function initializeMap() {
 
   var mapOptions = {
     disableDefaultUI: true,
-	backgroundColor: 'black',
-	draggable: true,
-	panControl: false,
-	scaleControl: false,
-	scrollwheel: false,
-	zoomControl: false
+  backgroundColor: 'black',
+  draggable: true,
+  panControl: false,
+  scaleControl: false,
+  scrollwheel: false,
+  zoomControl: false
   };
 
   /*
@@ -259,229 +488,3 @@ window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
   map.fitBounds(mapBounds);
 });
-
-// this is the start of resumebuilder.js
-
-//bio object to go to header
-var bio = {
-    "name": "Kaisha Ansley-Gutierrez",
-    "role": "Front End Web Developer",
-    "contacts": {
-        "mobile": "310-343-0537",
-        "email": "trojankai@gmail.com",
-        "github": "trojankai",
-        "twitter": "trojankai",
-        "location": "Whittier, CA"
-    },
-    "welcomeMessage": "I am Hardworking and Incredible",
-    "skills": ["HTML and CSS", "knitting and crocheting", "baby wranglin'" , "learning quickly", "customer service", "time management",  "bath renovations"],
-    "biopic": "images/profilePic.jpg",
-    display: function(){
-      var formattedName = HTMLheaderName.replace("%data%", bio.name);
-      var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-      var formattedNameRole = formattedName + formattedRole;
-      $("#header").prepend(formattedNameRole);
-
-      var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
-      var formattedWelcome = HTMLwelcomeMsg.replace('%data%',bio.welcomeMessage);
-
-
-      $("#header").append(formattedBioPic);
-
-      var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
-      $("#topContacts").append(formattedLocation);
-      $('#footerContacts').append(formattedLocation);
-
-      var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-      $("#topContacts").append(formattedEmail);
-      $('#footerContacts').append(formattedEmail);
-
-      var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
-      $("#topContacts").append(formattedGithub);
-      $('#footerContacts').append(formattedGithub);
-
-      $('#header').append(formattedWelcome);
-
-      $("#header").append(HTMLskillsStart);
-      for (var skill in bio.skills) {
-          var formattedSkills = HTMLskills.replace("%data%", bio.skills[skill]);
-           $("#skills-h3:last").append(formattedSkills);
-    }
-
-}
-};
-
-bio.display();
-
-//education object and dispaly function
-var education = {
-
-    "schools": [
-		{
-		"name": "University of Southern California",
-		"location": "Los Angeles, CA",
-		"degree": "B.A.",
-		"majors": ["Social Sciences emphasis Psychology"],
-		"dates": 'August 2001-Jan 2006 and Jan - May 2012',
-		"url": "www.usc.edu"
-		},
-		{
-		"name": "University of California, Irvine, Extension Program",
-		"majors": ["Light Construction and Development Management courses"],
-		"degree": "Certificate",
-    "location": "Irvine, CA",
-		"dates": 2009 + " - incomplete",
-		"url": "www.unex.uci.edu"
-
-		},
-		{
-		"name": "California State University, Dominguez Hills",
-		"majors": ["Project Management courses"],
-		"degree": "Certificate",
-		"location": "Carson, CA",
-		"dates": 2009 + " - complete program incomplete",
-		"url": "www.csudh.edu"
-		}
-	 ],
-	 "onlineCourses": [
-		{
-		"school": "Udacity",
-		"title": "Front End Web Development ",
-		"dates": "August 2015 - present",
-		"url": "www.udacity.com/nanodegree"
-		},
-		{
-		"school": "Coursera",
-		"title": "Introduction to Interactive Programming in Python",
-		"dates": "July 2015 - October 2015",
-		"url": "www.coursera.com"
-		}
-	],
-
-  displaySchools: function() {
-      for (var e = 0; e < education.schools.length; e++) {
-        $("#education").append(HTMLschoolStart);
-        var formattedName = HTMLschoolName.replace("%data%", education.schools[e].name);
-        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[e].location);
-        
-        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[e].degree);
-        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[e].dates);
-        $(".education-entry:last").append(formattedName + formattedDegree);
-        $(".education-entry:last").append(formattedDates);
-        $(".education-entry:last").append(formattedLocation);
-        var formattedMajors = HTMLschoolMajor.replace("%data%", education.schools[e].majors);
-        $(".education-entry:last").append(formattedMajors);
-      }
-    },
-    displayOnlineCourses: function() {
-      if (education.onlineCourses.length > 0) {
-        $("#education").append(HTMLonlineClasses);
-        for (var e = 0; e < education.onlineCourses.length; e++) {
-          $("#education").append(HTMLschoolStart);
-          var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[e].title);
-          formattedTitle = formattedTitle.replace("%url%", education.onlineCourses[e].url);
-          var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[e].school);
-          var formattedDates = HTMLonlineDates.replace("%data%", education.onlineCourses[e].dates);
-          var formattedURL = HTMLonlineURL.replace("%url%", education.onlineCourses[e].url);
-          $(".education-entry:last").append(formattedTitle + formattedSchool);
-          $(".education-entry:last").append(formattedDates);
-          $(".education-entry:last").append("<br>");
-        }
-      }
-    }
-  };
-
-  education.displaySchools();
-  education.displayOnlineCourses();
-
-
-//work object
-var work = {
-	"jobs": [
-		{
-		"employer":"University of California, Irvine",
-		"title": "Senior Building Maintenance Worker",
-		"location": "Irvine, CA",
-		"dates": "July 2007 to January 2014",
-		"description": "Perform general building maintenance, including carpentry, electrical, plumbing, drywall repair, painting, and basic HVAC.  Maintain good customer service, both internal and external. Report work completed both orally and written. Assist team members as needed."
-		},
-		{
-		"employer":"Bally Total Fitness",
-		"title": "(Acting) Supervisor, Member Services",
-		"location": "Norwalk, CA",
-		"dates": "Feb 2006 to June 2007",
-		"description": "Call center environment-handled member concerns, handled escalated calls. Assisted in the training and development of member service representatives. Responsible for motivating representatives to meet and maintain company/ departmental standards. Responsible for issuing corrective actions or terminating employees when necessary. Evaluated and corrected timesheets to ensure payroll completed on-time."
-		},
-		{
-		"employer": "USC Campus Cruiser",
-		"title": "Driver, Dispatcher, Trainer, Field Supervisor",
-		"location": "Los Angeles, CA",
-		"dates": "Oct 2002 to Jan 2006",
-		"description": "Trained incoming employees to work in the call center, as well as handle dispatched calls while maintaining impeccable customer service and providing safety to patrons. Opened and closed shifts, inspected vehicles for damage, patrolled field to insure that policies and procedures are properly followed by cruisers(drivers). Duties also included handling complaints and special calls, such as disabled patrons requiring wheelchair assistance."
-		}
-	],
-  display: function(){
-    $('#workExperience').append(HTMLworkStart);
-    for(var i=0; i<work.jobs.length; i++){
-      var formattedEmployer = HTMLworkEmployer.replace('%data%', work.jobs[i].employer);
-      var formattedTitle = HTMLworkTitle.replace('%data%',work.jobs[i].title);
-      var formattedEmployerTitle= formattedEmployer + formattedTitle;
-      var formattedDates = HTMLworkDates.replace('%data%', work.jobs[i].dates);
-      var formattedLocation = HTMLworkLocation.replace('%data%',work.jobs[i].location);
-      var formattedDescription = HTMLworkDescription.replace('%data%', work.jobs[i].description)
-      $('.work-entry:last').append(formattedEmployerTitle, formattedDates, formattedLocation, formattedDescription);
-    }
-
-  }
-};
-//display work function...
-
-work.display();
-
-
-//project object
-var projects = {
-	"project":[
-		{
-		"title":"Project 0",
-		"dates":"Aug 2015",
-		"description":"Started the journey of becoming a Front End Developer",
-		"images":["images/project1.jpg"]
-		},
-		{
-		"title":"Re-branding an Elementary School",
-		"dates":"Jan 2015",
-		"description":"Designed a new logo for Dolores Huerta Elementary school",
-		"images":["images/DHES-1.png"]
-		}
-	],
-  display: function(){
-    $('#projects').append(HTMLprojectStart);
-    for(var i = 0; i < projects.project.length; i++){
-      var formattedProjectName = HTMLprojectTitle.replace('%data%', projects.project[i].title);
-      var formattedProjectDates = HTMLprojectDates.replace('%data%', projects.project[i].dates);
-      var formattedProjectDescription = HTMLprojectDescription.replace('%data%', projects.project[i].description);
-      var formattedProjectImage = HTMLprojectImage.replace('%data%', projects.project[i].images);
-      $('.project-entry').append(formattedProjectName, formattedProjectDates, formattedProjectDescription, formattedProjectImage);
-    }
-
-
-  }
-};
-
-projects.display();
-
-
-//internationalize button code--linked above
-function inName(name) {
-    name = name.trim().split(" ");
-    console.log(name);
-    name[1] = name[1].toUpperCase();
-    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
-
-    return name[0] + " "+name[1];
-
-}
-
-//$('#main').append(internationalizeButton);
-$('#mapDiv').append(googleMap);
